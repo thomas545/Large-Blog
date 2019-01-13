e7fa1d1feebd52dbfa5175383d555f81c3d7fbdf
tomas2310328 <tomas.temo77@gmail.com>
2019-01-13 20:11:12 +0200

    CRUD Blog App

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
+from .views import (
+            PostListView,
+            PostDetailView,
+            PostCreateView,
+            PostUpdateView,
+            PostDeleteView
+)
 from . import views
 
 
 
 
 urlpatterns = [
-    path('' , views.home , name='home'),
+    path('' , PostListView.as_view() , name='home'),
+    path('post/create/' , PostCreateView.as_view() , name='create'),
+    path('post/<int:pk>/' , PostDetailView.as_view() , name='detail'),
+    path('post/<int:pk>/update/' , PostUpdateView.as_view() , name='update'),
+    path('post/<int:pk>/delete/' , PostDeleteView.as_view() , name='delete'),
     path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,57 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+class PostDetailView(DetailView):
+    model = Post
+   #post_detail.html
+
+
+class PostCreateView(LoginRequiredMixin , CreateView):
+    model = Post
+    fields = ['title' , 'content']
+
+    ## Permission to user own post only create post
+    def form_valid(self , form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin , UserPassesTestMixin , UpdateView):
+    model = Post
+    fields = ['title' , 'content']
+    ## Permission to user own post only update post
+    def form_valid(self , form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    ## Permission to user own post only update post
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author :
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin , UserPassesTestMixin , DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author :
+            return True
+        return False
 
 
 def about(request):
